3365e1599dc4724e6e3d0d1cf85fc5ef6e986f11
Andrea Gaither <angaither@gmail.com>
2014-07-23 12:39:04 -0400

    Basic logging of message broker transactions in mongo (locally)

--- app.js
+++ app.js
@@ -4,7 +4,14 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var amqp = require('amqp');
+var PHPUnserialize = require('php-unserialize');
 
+// Database
+var db = require('monk')('localhost:27017/drupal-logging');
+
+
+// Routes
 var routes = require('./routes/index');
 
 var app = express();
@@ -22,12 +29,56 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 
+
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
+
+
+
+
+// RabbitMQ
+var mb_config = require(__dirname + '/config/mb_config.json');
+var conn = amqp.createConnection({
+  host: mb_config.host,
+  port: mb_config.port,
+  login: mb_config.login,
+  password: mb_config.password,
+  connectionTimeout: mb_config.connectionTimeout,
+  authMechanism: mb_config.authMechanism,
+  vhost: mb_config.vhost,
+  noDelay: mb_config.noDelay,
+  ssl: { enabled : mb_config.ssl_enabled }
+},
+{
+  defaultExchangeName: mb_config.defaultExchangeName
+});
+ conn.on('ready', function(){
+  var q = conn.queue('activityStatsQueue', {
+    passive: mb_config.passive,
+    durable: mb_config.durable,
+    exclusive: mb_config.exclusive,
+    autoDelete: mb_config.autoDelete
+  }, function (q) {
+    q.bind('#');
+    q.subscribe(function (message) {
+      var messageObj = PHPUnserialize.unserialize(message.data);
+      var logs = db.get('logs');
+      logs.insert({
+        "activity" : messageObj.activity,
+        "uid" : messageObj.uid,
+        "email": messageObj.email,
+        "application_id": messageObj.application_id,
+        "activity_timestamp" : messageObj.activity_timestamp,
+      });
+    });
+  });
+});
+
+
 
 /// error handlers
 

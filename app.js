25cd7f8dd5267bac9575d5f2ec95eeb94bcedb8a
Andrea Gaither <angaither@gmail.com>
2014-07-25 11:46:48 -0400

    Removed display logic/dependencies. Set to watch correct queue.

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('static-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 var amqp = require('amqp');
 var PHPUnserialize = require('php-unserialize');
 
@@ -16,29 +13,7 @@ var routes = require('./routes/index');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(favicon());
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-
-
-/// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-
-
 
 // RabbitMQ
 var mb_config = require(__dirname + '/config/mb_config.json');
@@ -57,7 +32,7 @@ var conn = amqp.createConnection({
   defaultExchangeName: mb_config.defaultExchangeName
 });
  conn.on('ready', function(){
-  var q = conn.queue('activityStatsQueue', {
+  var q = conn.queue('loggingQueue', {
     passive: mb_config.passive,
     durable: mb_config.durable,
     exclusive: mb_config.exclusive,
@@ -71,8 +46,9 @@ var conn = amqp.createConnection({
         "activity" : messageObj.activity,
         "uid" : messageObj.uid,
         "email": messageObj.email,
-        "application_id": messageObj.application_id,
+        "fname" : messageObj.merge_vars.FNAME,
         "activity_timestamp" : messageObj.activity_timestamp,
+        "nid" : messageObj.eventID,
       });
     });
   });
